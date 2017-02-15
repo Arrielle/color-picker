05c9e2a51453fba30eb9386c9929f8c8c12ad6f1
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-15 09:27:34 -0600

    adding score counter and game restart on lose

--- client.js
+++ client.js
@@ -4,37 +4,107 @@ $(document).ready(function(){
   var colorArray = ['hotpink', 'deepskyblue', 'darkcyan', 'tan', 'coral', ];
 
   function randomNumber(min, max){
-      var randomNumberReturn = Math.floor(Math.random() * (1 + max - min) + min);
-      return randomNumberReturn;
+    var randomNumberReturn = Math.floor(Math.random() * (1 + max - min) + min);
+    return randomNumberReturn;
   }
   //checking to see if/how function is run
 
   var randomGeneratedNumber = randomNumber(0, colorArray.length - 1);
-    console.log(randomGeneratedNumber);
+  console.log(randomGeneratedNumber);
 
   for (var i = 0; i < colorArray.length; i++) {
     $('body').append('<div id = \'' + colorArray[i] + '\'></div>');
     $('#' + colorArray[i]).css({"background-color":colorArray[i], "float":"left" , "width":"150px", "height":"150px", "margin":"10px"});
   }
 
-  $('div').on('click', function(){
+  var yourScore = 0;
 
-      console.log($("div").index(this));
+  $('div').on('click', function(){
+    // console.log($("div").index(this));
+    //$(div).this().index(); ????
+    if ($("div").index(this) == randomGeneratedNumber){
+      $('h3').text('That is right! Your color was ' + colorArray[randomGeneratedNumber] + '!');
+      yourScore += 2;
+      $(this).css("background-color", "lime");
+      setTimeout(function(){
+        $('div:nth-of-type(' + (randomGeneratedNumber + 1) + ')').css("background-color", colorArray[randomGeneratedNumber]);
+        randomGeneratedNumber = randomNumber(0, colorArray.length - 1);
+      }, 1500)
+      //your right click another color to play again
+
+    } else {
+      $('h3').text('Nope! That\'s not the right color. Try again.');
+      yourScore -= 1;
+    }
+
+    $('h2').text("Your Score Is: " + yourScore);
+
+    if (yourScore < -10) {
+      $('div').remove();
+      $('h3').empty();
+      $('h3').text("Sorry, you lost.");
+      var newButton = $('<button>')
+      newButton.text('PLAY AGAIN');
+      $('body').append(newButton);
+    };
+
+  });
+
+  //restart game
+
+  $('body').on('click', 'button', function(){
+
+    $('h3').empty();
+    $('h2').empty();
+    $('button').remove();
+    console.log('button was clicked');
+
+    for (var i = 0; i < colorArray.length; i++) {
+      $('body').append('<div id = \'' + colorArray[i] + '\'></div>');
+      $('#' + colorArray[i]).css({"background-color":colorArray[i], "float":"left" , "width":"150px", "height":"150px", "margin":"10px"});
+    }
+
+    randomNumber(0, colorArray.length - 1);
+
+    yourScore = 0;
+
+    console.log(randomGeneratedNumber);
+
+    $('div').on('click', function(){
+      // console.log($("div").index(this));
       //$(div).this().index(); ????
       if ($("div").index(this) == randomGeneratedNumber){
         $('h3').text('That is right! Your color was ' + colorArray[randomGeneratedNumber] + '!');
-        $(this).css("background-color", "yellow");
+        yourScore += 2;
+        $(this).css("background-color", "lime");
         setTimeout(function(){
           $('div:nth-of-type(' + (randomGeneratedNumber + 1) + ')').css("background-color", colorArray[randomGeneratedNumber]);
           randomGeneratedNumber = randomNumber(0, colorArray.length - 1);
-        }, 2000)
+        }, 1500)
         //your right click another color to play again
 
       } else {
-        $('h3').text('Nope! That is not the right color. Try again.');
+        $('h3').text('Nope! That\'s not the right color. Try again.');
+        yourScore -= 1;
       }
 
-  });//ends onclick function
+      $('h2').text("Your Score Is: " + yourScore);
+
+      if (yourScore < -10) {
+        $('div').remove();
+        $('h3').empty();
+        $('h3').text("Sorry, you lost.");
+        var newButton = $('<button>')
+        newButton.text('PLAY AGAIN');
+        $('body').append(newButton);
+      };//ends if
+
+    }); // ends on click
+
+  });//ends button on click
+
+
+  //ends onclick function//ends onclick function
   // var colorArray = ['pink', 'blue', 'green', 'tan', 'orange'];
   //
   // for (var i = 0; i < array.length; i++) {
@@ -50,6 +120,7 @@ $(document).ready(function(){
   // for (var i = 0; i < array.leng; i++) {
   //
   // }
+
 
 
 
